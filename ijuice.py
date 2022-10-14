61c07f3ab6b6dfb022f2ad5d75f117e86def517d
alku7660 <alejandro.kuratomi@dsv.su.se>
2022-10-14 16:48:34 +0200

    Added NN and data files

--- ijuice.py
+++ ijuice.py
@@ -1,2 +1,12 @@
 import numpy as np
 
+
+class Ijuice:
+
+    def __init__(self, data, ioi, model):
+        self.name = data.name
+        self.ioi_idx = ioi.idx
+        self.ioi = ioi.x
+        self.normal_ioi = ioi.normal_ioi
+        self.ioi_label = ioi.label
+        self.nn_cf = 
