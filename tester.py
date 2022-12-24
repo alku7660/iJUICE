b15e6c4914175d8faeede88187e56b378871aaad
alku7660 <alejandro.kuratomi@dsv.su.se>
2022-12-24 16:11:56 +0100

    Ready for obtaining other methods CFs

--- tester.py
+++ tester.py
@@ -7,7 +7,7 @@ from evaluator_constructor import Evaluator
 from counterfactual_constructor import Counterfactual
 from address import save_obj, load_obj, results_obj
 
-datasets = ['adult','kdd_census','german','dutch','bank','credit','compass','diabetes','ionosphere','student','oulad','law','heart','synthetic_athlete','synthetic_disease'] # ['adult','kdd_census','german','dutch','bank','credit','compass','diabetes','ionosphere','student','oulad','law','heart','synthetic_athlete','synthetic_disease']
+datasets = ['adult','kdd_census','german','dutch','bank','credit','diabetes','ionosphere','student','oulad','law','heart','synthetic_athlete','synthetic_disease'] # ['adult','kdd_census','german','dutch','bank','credit','compass','diabetes','ionosphere','student','oulad','law','heart','synthetic_athlete','synthetic_disease']
 methods = ['mace'] # ['nn','mo','ft','rt','gs','face','dice','mace','cchvae','juice','ijuice']
 seed_int = 54321
 step = 0.01
