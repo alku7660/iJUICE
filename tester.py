e3a45efd81f2bc51c87cf8e5b7b01158a8ecfdaf
alku7660 <alejandro.kuratomi@dsv.su.se>
2023-01-09 15:27:00 +0100

    Update tester.py

--- tester.py
+++ tester.py
@@ -7,15 +7,15 @@ from evaluator_constructor import Evaluator
 from counterfactual_constructor import Counterfactual
 from address import save_obj, load_obj, results_obj
 
-datasets = ['diabetes'] # ['adult','kdd_census','german','dutch','bank','credit','compass','diabetes','ionosphere','student','oulad','law','heart','synthetic_athlete','synthetic_disease']
-methods = ['nn'] # ['nn','mo','ft','rt','gs','face','dice','mace','cchvae','juice','ijuice']
+datasets = ['adult','kdd_census','german','dutch','bank','credit','compass','diabetes','student','oulad','law','heart','synthetic_athlete','synthetic_disease'] # ['adult','kdd_census','german','dutch','bank','credit','compass','diabetes','student','oulad','law','heart','synthetic_athlete','synthetic_disease']
+methods = ['juice','ijuice'] # ['nn','mo','ft','rt','gs','face','dice','mace','cchvae','juice','ijuice']
 seed_int = 54321
 step = 0.01
 train_fraction = 0.7
 distance_type = ['euclidean'] # ['euclidean','L1','L_inf','L1_L0','L1_L0_L_inf','prob']
 lagranges = [1]    # np.linspace(start=0, stop=1, num=11)
-num_instances = 60 # data.test_df.shape[0]
-prepare_for_mace = True
+num_instances = 20 # data.test_df.shape[0]
+prepare_for_mace = False
 
 """
 Instructions to run tests:
