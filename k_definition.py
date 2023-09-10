6a7b996015db972ddb38953c761effcec959bd26
alku7660 <alejandro.kuratomi@dsv.su.se>
2023-09-10 17:16:00 +0200

    diabetes

--- k_definition.py
+++ k_definition.py
@@ -254,7 +254,7 @@ def single_justification_anomaly(data_str, distance, num_instances=None):
     model = Model(data)
     data.undesired_test(model)
     if num_instances is None:
-        num_instances = data.undesired_transformed_test_df.shape[0] if data.undesired_transformed_test_df.shape[0] < 101 else 100
+        num_instances = data.undesired_transformed_test_df.shape[0] if data.undesired_transformed_test_df.shape[0] < 126 else 125
     X_desired = get_desired_class_training_instances(data)
     X_desired_likelihood = desired_class_training_outliers(X_desired)
     indices_outliers = find_outliers_with_threshold(X_desired, X_desired_likelihood)
@@ -283,7 +283,7 @@ def store_anomaly_justification_result(distance):
     """
     Method that stores the results of the anomaly justification ratio study
     """
-    datasets = ['oulad'] # 'kdd_census','dutch','credit','oulad'
+    datasets = ['diabetes'] # 'kdd_census','dutch','credit','oulad'
     for data_str in datasets:
         ratio_outliers = {}
         ratio_outlier_justification = single_justification_anomaly(data_str, distance)
