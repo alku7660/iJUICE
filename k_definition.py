6db78f808d92f74b90be5c4b30c807bb71865548
alku7660 <alejandro.kuratomi@dsv.su.se>
2023-09-10 17:16:24 +0200

    Update k_definition.py

--- k_definition.py
+++ k_definition.py
@@ -283,7 +283,7 @@ def store_anomaly_justification_result(distance):
     """
     Method that stores the results of the anomaly justification ratio study
     """
-    datasets = ['diabetes'] # 'kdd_census','dutch','credit','oulad'
+    datasets = ['diabetes'] # 'kdd_census','dutch','credit'
     for data_str in datasets:
         ratio_outliers = {}
         ratio_outlier_justification = single_justification_anomaly(data_str, distance)
