89d56b323c7ed522f6d246bf6cace986f9f6e403
alku7660 <alejandro.kuratomi@dsv.su.se>
2023-09-15 18:55:27 +0200

    Ready to rerun some experiments

--- k_definition.py
+++ k_definition.py
@@ -291,12 +291,12 @@ def store_anomaly_justification_result(distance):
         df_ratio_outliers = pd.DataFrame.from_dict(ratio_outliers)
         df_ratio_outliers.to_csv(results_k_definition+f'{data_str}_ratio_outlier_justification.csv')
 
-idx = 0 # 150 for synthetic_2d, 0 for the others
-data_str = 'oulad' # 'synthetic_2d', 'dutch', 'diabetes', 'oulad', 'athlete'
-distance = 'L1_L0' # 'euclidean', 'L1_L0'
-range_k_values = range(1, 31) # 'range(1, 58)', 'range(1, 21)' 
-# ijuice_varying_k(data_str, distance, range_k_values)
-store_anomaly_justification_result(distance)
+idx = 150 # 150 for synthetic_2d, 0 for the others
+data_str = 'synthetic_2d' # 'synthetic_2d', 'dutch', 'diabetes', 'oulad', 'athlete'
+distance = 'euclidean' # 'euclidean', 'L1_L0'
+range_k_values = range(1, 16) # 'range(1, 58)', 'range(1, 21)' 
+ijuice_varying_k(data_str, distance, range_k_values, idx)
+# store_anomaly_justification_result(distance)
 
 # store_data_set(seed_int)
 # X, Y = training_set(seed_int)
