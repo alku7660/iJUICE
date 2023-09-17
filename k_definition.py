9ade033eb544d0040720784788127fcd98c43b36
alku7660 <alejandro.kuratomi@dsv.su.se>
2023-09-17 17:05:09 +0200

    Update k_definition.py

--- k_definition.py
+++ k_definition.py
@@ -303,34 +303,3 @@ ijuice_varying_k(data_str, distance, range_k_values)
 # f = train_model(X, Y, seed_int)
 # ioi = point_of_interest()
 # plot_dataset(f, X, Y, ioi)
-
-# datasets = ['adult','kdd_census','diabetes','oulad',] # ,'adult','kdd_census','diabetes','oulad','german','dutch','bank','credit','compass','diabetes','student','oulad','law','heart','synthetic_disease'
-# distance_type = ['L1_L0','L1_L0_L_inf','prob']
-
-# for data_str in datasets:
-#     data = load_dataset(data_str, train_fraction, seed_int, step)
-#     idx_counterfactuals = get_idx_cf(data)
-#     save_obj(idx_counterfactuals, results_k_definition, f'idx_counterfactuals_{data_str}')
-#     for type in distance_type:
-#         dist = calculate_cf_train_distance_matrix(data, idx_counterfactuals, type)
-#         save_obj(dist, results_k_definition, f'dist_matrix_{data_str}_{type}')
-#         print(f'Dataset: {data_str}, Distance: {type}, Total instances: {data.train_target[np.where(data.train_target == 1 - data.undesired_class)[0]].shape[0]}')
-
-
-# for data_str in datasets:
-#     data = load_dataset(data_str, train_fraction, seed_int, step)
-#     total_cf_instances = data.train_target[np.where(data.train_target == 1 - data.undesired_class)[0]].shape[0]
-#     range_neighbors = range(int(total_cf_instances*0.05), int(total_cf_instances*0.2))
-#     pandas_results_type = pd.DataFrame(index=distance_type, columns=range_neighbors)
-#     for type in distance_type:
-#         for neighbors in range_neighbors:
-#             outliers = estimate_outliers(data, type, neighbors)
-#             print(f'Dataset: {data_str}, Distance: {type}, Neihgbors: {neighbors}, Total instances: {total_cf_instances}, Number of outliers: {len(outliers)}')
-#             suggested_k = len(outliers) + 1
-#             pandas_results_type.loc[type, neighbors] = suggested_k
-#     pandas_results_type.to_csv(results_k_definition+'k_definition.csv')
-
-
-
-
-
